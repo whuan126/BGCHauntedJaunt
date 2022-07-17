5372b4841588a495b4b4bf2e622d29960bdf2d0e
whuan126 <whuan126@ucr.edu>
2022-07-17 12:12:36 -0700

    Finished audio implementation
    Added audio for John Lemon's footsteps, ghost ambient noise adjusted for distance.

--- LemonProject/Assets/Scripts/WaypointControl.cs
+++ LemonProject/Assets/Scripts/WaypointControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class WaypointPatrol : MonoBehaviour
+public class WaypointControl : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
