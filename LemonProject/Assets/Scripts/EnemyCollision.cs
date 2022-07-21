365ff138210a49a198eaf2db6b8b919a76b47843
Will Huang <82676480+whuan126@users.noreply.github.com>
2022-07-21 10:43:58 -0700

    Added game ending collision to ghost
    Game now ends if John Lemon comes in physical contact with a ghost.

---
+++ LemonProject/Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    public Transform player;
+    bool m_IsPlayerTouching;
+    public GameEnding gameEnding;
+
+    void OnTriggerEnter(Collider collide)
+    {
+        if (collide.gameObject.tag == "Player")
+        {
+            Debug.Log("Player is touching");
+            m_IsPlayerTouching = true;
+        }
+    }
+    void OnTriggerExit(Collider collide)
+    {
+        if (collide.gameObject.tag == "Player")
+        {
+            Debug.Log("Player is NOT touching");
+            m_IsPlayerTouching = false;
+        }
+    }
+
+    void Update()
+    {
+        if(m_IsPlayerTouching)
+        {
+            Debug.Log("John Lemon is caught!");
+            gameEnding.CaughtPlayer();
+        }
+    }
+
+}
